bcab42ae1305042a408a96d24008385d39973304
vijay <bandivijay.vk@gmail.com>
2021-08-24 12:50:39 +0530

    new1

--- src/vijay/Project1.java
+++ src/vijay/Project1.java
@@ -5,6 +5,9 @@ public class Project1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome");
+
+System.out.println("welcome");
+System.out.println("welcome");
 	}
 
 }
